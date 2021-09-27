946e97bb999c3936c4927ffb369eb65dacadb72b
Ludovic Vanasse <ludovicvanasse@gmail.com>
2021-09-27 11:52:22 -0400

    Add SetNeutralMode to brake

--- markhor_tracks/src/markhor_hw_interface.cpp
+++ markhor_tracks/src/markhor_hw_interface.cpp
@@ -55,20 +55,24 @@ void MarkhorHWInterface::setupCTREDrive()
   if (nh.getParam("/markhor/markhor_tracks_node/front_left", drive_fl_id) == true)
   {
     front_left_drive = std::make_unique<TalonSRX>(drive_fl_id);
+    front_left_drive->SetNeutralMode(NeutralMode::Brake);
     front_left_drive->SetInverted(true);  // Fix drive orientation
   }
   if (nh.getParam("/markhor/markhor_tracks_node/rear_left", drive_rl_id) == true)
   {
     rear_left_drive = std::make_unique<TalonSRX>(drive_rl_id);
+    rear_left_drive->SetNeutralMode(NeutralMode::Brake);
     rear_left_drive->SetInverted(true);  // Fix drive orientation
   }
   if (nh.getParam("/markhor/markhor_tracks_node/front_right", drive_fr_id) == true)
   {
     front_right_drive = std::make_unique<TalonSRX>(drive_fr_id);
+    front_right_drive->SetNeutralMode(NeutralMode::Brake);
   }
   if (nh.getParam("/markhor/markhor_tracks_node/rear_right", drive_rr_id) == true)
   {
     rear_right_drive = std::make_unique<TalonSRX>(drive_rr_id);
+    rear_right_drive->SetNeutralMode(NeutralMode::Brake);
   }
 }
 
