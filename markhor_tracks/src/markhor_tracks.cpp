5623bf34acbed2f116206257f3bb2e5fb4f9caf5
Ludovic Vanasse <ludovicvanasse@gmail.com>
2022-01-30 16:22:28 -0500

    Change escape char from string to int

--- markhor_tracks/src/markhor_tracks.cpp
+++ markhor_tracks/src/markhor_tracks.cpp
@@ -20,31 +20,31 @@ int main(int argc, char** argv)
 {
    ros::init(argc, argv, "markhor_tracks_node");
 
-  ROS_INFO("LUDO %s",__LINE__);
+  ROS_INFO("LUDO %d",__LINE__);
   MarkhorHWInterface hw;
-    ROS_INFO("LUDO %s",__LINE__);
+    ROS_INFO("LUDO %d",__LINE__);
   controller_manager::ControllerManager cm(&hw, hw.nh);
 
-  ROS_INFO("LUDO %s",__LINE__);
+  ROS_INFO("LUDO %d",__LINE__);
   double control_frequency;
-  ROS_INFO("LUDO %s",__LINE__);
+  ROS_INFO("LUDO %d",__LINE__);
   hw.private_nh.param<double>("control_frequency", control_frequency, 60.0);
 
-  ROS_INFO("LUDO %s",__LINE__);
+  ROS_INFO("LUDO %d",__LINE__);
   ros::CallbackQueue my_robot_queue;
   ros::AsyncSpinner my_robot_spinner(1, &my_robot_queue);
 
-  ROS_INFO("LUDO %s",__LINE__);
+  ROS_INFO("LUDO %d",__LINE__);
   std::chrono::system_clock::time_point last_time = std::chrono::system_clock::now();
-  ROS_INFO("LUDO %s",__LINE__);
+  ROS_INFO("LUDO %d",__LINE__);
   ros::TimerOptions control_timer(ros::Duration(1 / control_frequency),
                                   std::bind(controlLoop, std::ref(hw), std::ref(cm), std::ref(last_time)),
                                   &my_robot_queue);
-  ROS_INFO("LUDO %s",__LINE__);
+  ROS_INFO("LUDO %d",__LINE__);
   ros::Timer control_loop = hw.nh.createTimer(control_timer);
-  ROS_INFO("LUDO %s",__LINE__);
+  ROS_INFO("LUDO %d",__LINE__);
   my_robot_spinner.start();
-  ROS_INFO("LUDO %s",__LINE__);
+  ROS_INFO("LUDO %d",__LINE__);
   ros::spin();
 
   return 0;
