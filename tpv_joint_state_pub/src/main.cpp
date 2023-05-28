d7f0c191d7ffb7a588f68cb508b55ceacca5b857
Benoit Malenfant <benmalenfant@gmail.com>
2023-05-28 10:42:43 -0700

    Add topic read

--- tpv_joint_state_pub/src/main.cpp
+++ tpv_joint_state_pub/src/main.cpp
@@ -1,35 +1,50 @@
 #include "ros/ros.h"
 #include "sensor_msgs/JointState.h"
+#include "std_msgs/Float64.h"
+#include "math.h"
+
+#define X_REDUCTION 0.25
+#define DEG_TO_RAD 180.0/M_PI
+
+sensor_msgs::JointState joint_state_msg;
+
+void callback_x(const std_msgs::Float64::ConstPtr& msg)
+{
+    joint_state_msg.position[0] = msg->data*DEG_TO_RAD*X_REDUCTION;
+}
+
+void callback_y(const std_msgs::Float64::ConstPtr& msg)
+{
+    joint_state_msg.position[1] = msg->data*DEG_TO_RAD;
+}
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "talker");
-    ros::NodeHandle n;
+    ros::NodeHandle nh;
 
-    ros::Publisher state_pub = n.advertise<sensor_msgs::JointState>("joint_states", 1);
+    ros::Publisher state_pub = nh.advertise<sensor_msgs::JointState>("joint_states", 1);
 
-    sensor_msgs::JointState msg;
-    msg.name.push_back("tpv_base_j");
-    msg.name.push_back("tpv_tilt_j");
-    msg.position.push_back(0.0);  // Initial position of joint1
-    msg.position.push_back(0.0);
+    sensor_msgs::JointState joint_state_msg;
+    joint_state_msg.name.push_back("tpv_base_j");
+    joint_state_msg.name.push_back("tpv_tilt_j");
+    joint_state_msg.position.push_back(0.0); 
+    joint_state_msg.position.push_back(0.0);
+
+    ros::Subscriber sub_x = nh.subscribe("tpv_pos_x", 1, callback_x);
+    ros::Subscriber sub_y = nh.subscribe("tpv_pos_y", 1, callback_y);
 
     ros::Rate loop_rate(10);
 
-    double time = 0.0;
-
     while (ros::ok())
     {
-        msg.header.stamp = ros::Time::now();
-        msg.position[0] = 3.14*std::sin(time*0.5);  // Update position of joint1
-        msg.position[1] = 3.14*std::cos(time*0.5);
-        
-        state_pub.publish(msg);
+        joint_state_msg.header.stamp = ros::Time::now();
+
+        state_pub.publish(joint_state_msg);
 
         ros::spinOnce();
 
         loop_rate.sleep();
-        time += 0.1;
     }
 
     return 0;
