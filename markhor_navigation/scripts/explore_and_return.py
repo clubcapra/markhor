f8fbd217c36a1236590c3f08b29fa528e0b23d0d
SimonR99 <simonroy99@hotmail.ca>
2023-06-10 16:09:57 +0200

    add frontier exploration service

--- markhor_navigation/scripts/explore_and_return.py
+++ markhor_navigation/scripts/explore_and_return.py
@@ -6,47 +6,37 @@ import time
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 import actionlib
 from nav_msgs.msg import Odometry
-from markhor_navigation.srv import SetReturnPosition, SetReturnPositionResponse
+from markhor_navigation.srv import StartExploration, StartExplorationResponse
 from geometry_msgs.msg import Pose
 
-# Global variable to store the return position
-return_position = None
 
-def handle_set_return_position(req):
-    global return_position
-    return_position = req.return_position
-    return SetReturnPositionResponse(True, "Return position set successfully")
+def handle_start_exploration(req):
+    timeout = req.timeout
 
-def explore_and_return():
-    # Initialize the ros node
-    rospy.init_node('explore_and_return', anonymous=True)
+    # Get the current position to use as the return position
+    rospy.loginfo("Getting current position to use as return position...")
+    current_pose_msg = rospy.wait_for_message('/markhor/odometry/filtered', Odometry)
+    return_position = current_pose_msg.pose.pose
 
     # Create action client for move_base
+    rospy.loginfo("Connecting to move_base server...")
     client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
     client.wait_for_server()
     rospy.loginfo("Connected to move_base server")
-
-    # Get initial position
-    initial_pose_msg = rospy.wait_for_message('/markhor/odometry/filtered', Odometry)
-    initial_pose = initial_pose_msg.pose.pose
-    rospy.loginfo("Initial position: {}".format(initial_pose))
 
     # Start explore_lite node
     rospy.loginfo("Starting exploration...")
     explore_process = subprocess.Popen(["roslaunch", "explore_lite", "explore_costmap.launch"])
 
-    # Wait for specified amount of time
-    time.sleep(60)  # replace 60 with the amount of time you want to explore
+    # Wait for the specified amount of time
+    rospy.loginfo("Exploring for {} seconds...".format(timeout))
+    time.sleep(timeout)
 
     # Stop explore_lite node
     rospy.loginfo("Stopping exploration...")
     explore_process.terminate()
 
-    # If no return position was set, use the initial position
-    if return_position is None:
-        return_position = initial_pose
-
-    # Send goal to move_base to move back to the initial position
+    # Send goal to move_base to move back to the return position
     goal = MoveBaseGoal()
     goal.target_pose.header.frame_id = 'map'  # assuming you're using map frame
     goal.target_pose.header.stamp = rospy.Time.now()
@@ -58,11 +48,17 @@ def explore_and_return():
     # Wait for the action to complete
     client.wait_for_result()
 
+    return StartExplorationResponse(True, "Exploration started and return position set successfully")
+
 if __name__ == '__main__':
     try:
-        # Start the service server
-        rospy.Service('set_return_position', SetReturnPosition, handle_set_return_position)
+        # Initialize the ros node
+        rospy.init_node('explore_and_return', anonymous=True)
 
-        explore_and_return()
+        # Start the service server
+        rospy.Service('start_exploration', StartExploration, handle_start_exploration)
+
+        # Keep the node running until it's shut down
+        rospy.spin()
     except rospy.ROSInterruptException:
         pass
