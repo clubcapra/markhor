6a0eedc4a42b41ac4338d79cc4c8d3a59eeef5f0
GLDuval <glevesqueduval@gmail.com>
2023-06-14 10:55:06 +0200

    Add navigation heartbeat logic

--- markhor_heartbeat/src/heartbeat.cpp
+++ markhor_heartbeat/src/heartbeat.cpp
@@ -1,72 +1,32 @@
 #include <ros/ros.h>
 #include <std_srvs/SetBool.h>
 #include <std_msgs/String.h>
-#include <geometry_msgs/PoseStamped.h>
-#include <move_base_msgs/MoveBaseAction.h>
-#include <actionlib/client/simple_action_client.h>
 #include <chrono>
+#include <std_srvs/Trigger.h>
 
-bool lost_connection = false;
-bool autonomy_mode = false;
-int heartbeat_timeout;
+int ui_timeout;
+int navigation_timeout;
 int heartbeat_interval;
-int reconnection_attempts = 0;
+std::map<std::string, std::chrono::_V2::system_clock::time_point> last_heartbeats;
 std::chrono::_V2::system_clock::time_point last_heartbeat = std::chrono::system_clock::now();
 
 void heartbeat(const std_msgs::String message)
 {
-  last_heartbeat = std::chrono::system_clock::now();
+  map[message] = std::chrono::system_clock::now();
 }
 
 // Function that returns to known connection points until the heartbeat is back.
-bool return_to_connection_pose(int attempts = 0)
+bool return_to_last_connection_point()
 {
-  MoveBaseClient ac("move_base", true);
-  while (!ac.waitForServer(ros::Duration(5.0)))
-  {
-    ROS_INFO("Waiting for the move_base action server to come up");
-  }
-
-  // Get last connection point
-  ros::NodeHandle nh;
-  geometry_msgs::PoseStamped connection_pose;
-
-  move_base_msgs::MoveBaseGoal goal;
-  goal.target_pose.header.frame_id = "map";
-  goal.target_pose.header.stamp = ros::Time::now();
-
-  ac.sendGoal(goal);
-
-  // Wait for the action to finish
-  ac.waitForResult();
-
-  if (ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-  {
-    ROS_INFO("Robot has reached last known connection point.");
-    // Check again if we received a heartbeat after the robot has reached destination.
-    if(validate_heartbeat()){
-      lost_connection = false;
-    }else{
-      // If we still don't have a heartbeat, try again to go back to a later known connection point.
-      return_to_connection_pose(++attempts);
-    }
-  }
-  else
-  {
-    ROS_INFO("The robot failed to move to the last known connection point.");
-    std_srvs::Trigger service;
-      if (ros::service::exists("/markhor/estop_disable", true))
-      {
-        ros::service::call("/markhor/estop_disable", service);
-      }
-    return false;
-  }
+  // Call service
+  return true;
 }
 
-bool validate_heartbeat(){
+bool validate_heartbeat(std::chrono::_V2::system_clock::time_point last_heartbeat, int timeout)
+{
   std::chrono::_V2::system_clock::time_point now = std::chrono::system_clock::now();
   std::chrono::duration<double, std::milli> since_last_heartbeat = now - last_heartbeat;
-  if (since_last_heartbeat.count() > heartbeat_timeout)
+  if (since_last_heartbeat.count() > timeout)
   {
     return false;
   }
@@ -78,36 +38,16 @@ bool validate_heartbeat(){
 
 void check_heartbeat()
 {
-  bool heartbeat_valid = validate_heartbeat();
-  // If we lost the connection, bring back robot to last known connection point
-  if (!autonomy_mode && !lost_connection && !heartbeat_valid)
+  if (!validate_heartbeat(last_heartbeats.at("navigation"), navigation_timeout) &&
+      !validate_heartbeat(last_heartbeats.at("ui"), ui_timeout))
   {
-    ROS_WARN("Heartbeat timeout! Returning to last known connection point.");
-    lost_connection = true;
-    // Go back to known connection points until the heartbeat is back.
-    return_to_connection_pose();
+    bool success = return_to_connection_pose();
+    std_srvs::Trigger service;
+    if (ros::service::exists("/markhor/estop_disable", true))
+    {
+      ros::service::call("/markhor/estop_disable", service);
+    }
   }
-  else if(lost_connection && since_last_heartbeat.count() < heartbeat_timeout){
-    lost_connection = false;
-  }
-  
-}
-
-// Allow to toggle autonomy mode. If autonomy mode is enabled, the robot will not be stopped if the heartbeat is lost.
-bool toggleAutonomyMode(std_srvs::SetBool::Request& req, std_srvs::SetBool::Response& res)
-{
-  autonomy_mode = req.data;
-  if (autonomy_mode)
-  {
-    res.message = "Autonomy mode enabled.";
-  }
-  else
-  {
-    res.message = "Autonomy mode disabled.";
-  }
-
-  res.success = static_cast<unsigned char>(true);
-  return true;
 }
 
 int main(int argc, char* argv[])
@@ -117,16 +57,21 @@ int main(int argc, char* argv[])
 
   // Get parameters from launch file.
   nh.getParam("/markhor/heartbeat/markhor_heartbeat_node/heartbeat_interval", heartbeat_interval);
-  nh.getParam("/markhor/heartbeat/markhor_heartbeat_node/heartbeat_timeout", heartbeat_timeout);
+  nh.getParam("/markhor/heartbeat/markhor_heartbeat_node/navigation_timeout", navigation_timeout);
+  nh.getParam("/markhor/heartbeat/markhor_heartbeat_node/ui_timeout", ui_timeout);
 
   ROS_INFO("Heartbeat interval is : %d ms", heartbeat_interval);
-  ROS_INFO("Heartbeat timeout is : %d ms", heartbeat_timeout);
+  ROS_INFO("Navigation timeout is : %d ms", navigation_timeout);
+   ROS_INFO("UI timeout is : %d ms", ui_timeout);
+
+  // Init heartbeats
+  map["navigation"] = std::chrono::system_clock::now();
+  map["ui"] = std::chrono::system_clock::now();
 
   // Subscribe to heartbeat topic to receive heartbeats from web ui.
-  ros::Subscriber heartbeat_subscriber = nh.subscribe("/heartbeat", 5, heartbeat);
-  ros::ServiceServer toggle_autonomy_mode = nh.advertiseService("/heartbeat/toggle_autonomy_mode", toggleAutonomyMode);
+  ros::Subscriber heartbeat_subscriber = nh.subscribe("heartbeat", 5, heartbeat);
 
-  // Create timer to verify that we received a heartbeat.
+  // Timer for UI heartbeat
   ros::Timer timer1 = nh.createTimer(ros::Duration(heartbeat_interval / 1000), check_heartbeat);
 
   ros::spin();
